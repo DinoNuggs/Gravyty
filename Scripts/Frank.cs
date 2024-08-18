a06e4c5c2e9eff72a22d2a069710e877df19dfdb
Atrej Mak <mak006@cougars.csusm.edu>
2024-08-18 14:36:09 -0400

    using logmath to alter franks animation and speeds

--- Scripts/Frank.cs
+++ Scripts/Frank.cs
@@ -37,28 +37,29 @@ public partial class Frank : CharacterBody2D
 	{
 		headClearanceRay = GetNode<RayCast2D>("HeadRay");
 		animator = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		targetScale = Scale.Length();
+		targetScale = Scale.X;
 		weight = Scale.Length()*Scale.Length() * mass;
 	}
 	public override void _PhysicsProcess(double delta)
 	{
 		// GD.Print("Current Weight: " + weight);
 		Vector2 velocity = Velocity;
-		float scaleModifier = Scale.Length()/1.4142135f;
+		float scaleModifier = (float) Math.Log10(Scale.X/1.414) + 1;
+		//float scaleModifier = Scale.X/1.4142135f;
+		
 		weight = Scale.Length()*Scale.Length() * mass;
 		canScaleUp = true;
-		targetScale = Math.Clamp(targetScale, mininumScale, maximumScale);
-
+		
 		if (headClearanceRay.IsColliding()) {
 			//Cant scale if bonking above his head
 			canScaleUp = false;
-			targetScale = Scale.Length();
+			targetScale = Scale.X;
 		}
 
 		if ( leftBlocked && rightBlocked ) {
 			//Cant scale if frank is squeezed on both sides
 			canScaleUp = false;
-			targetScale = Scale.Length();
+			targetScale = Scale.X;
 		}
 
 		if (Input.IsActionJustReleased("scale_up")) {
@@ -71,11 +72,14 @@ public partial class Frank : CharacterBody2D
 			targetScale *= 0.8f;
 		}
 
-		if (targetScale > Scale.Length() && canScaleUp) {
+		targetScale = Math.Clamp(targetScale, mininumScale, maximumScale);
+		GD.Print("Scale: ", Scale, "\tTarget scale: ", targetScale, "\tScale modifier: ", scaleModifier);
+
+		if (targetScale > Scale.X && canScaleUp) {
 			Scale += new Vector2(ScalingRate * (float)delta, ScalingRate * (float)delta);
 		}
 
-		if (targetScale < Scale.Length()) {
+		if (targetScale < Scale.X) {
 			Scale -= new Vector2(ScalingRate * (float)delta, ScalingRate * (float)delta);
 		}
 
@@ -102,7 +106,7 @@ public partial class Frank : CharacterBody2D
 
 		// Grounded
 		if (IsOnFloor()) {
-			animator.SpeedScale = 1/Scale.X;
+			animator.SpeedScale = 1/scaleModifier;
 			if (direction != Vector2.Zero)
 			{
 				animator.Play("walk");
@@ -163,28 +167,24 @@ public partial class Frank : CharacterBody2D
 	}
 
 	public void OnLeftDetected(Node2D body) {
-		GD.Print("left detected: ", body.Name);
 		if(body.Name != "Frank") {
 			leftBlocked = true;
 		}
 	}
 
 	public void OnLeftExitDetected(Node2D body) {
-		GD.Print("left detected: ", body.Name);
 		if(body.Name != "Frank") {
 			leftBlocked = false;
 		}
 	}
 
 	public void OnRightDetected(Node2D body) {
-		GD.Print("right detected: ", body.Name);
 		if(body.Name != "Frank") {
 			rightBlocked = true;
 		}
 	}
 
 	public void OnRightExitDetected(Node2D body) {
-		GD.Print("right detected: ", body.Name);
 		if(body.Name != "Frank") {
 			rightBlocked = false;
 		}
